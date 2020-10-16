b531943919a213b27abbca93829decfb5778170c
Frank Cheng <ufo22940268@gmail.com>
2020-10-16 14:44:59 +0800

    v

---
+++ Zelda/EndPointDetailViewController.swift
@@ -0,0 +1,17 @@
+//
+//  EndPointDetailViewController.swift
+//  Zelda
+//
+//  Created by Frank Cheng on 2020/10/16.
+//
+
+import Cocoa
+
+class EndPointDetailViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do view setup here.
+    }
+    
+}
