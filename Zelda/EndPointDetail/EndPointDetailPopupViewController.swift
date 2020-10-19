f8cbbf9bdd2ea5ea62ab2afe8094764d94e45d42
Frank Cheng <ufo22940268@gmail.com>
2020-10-20 07:40:12 +0800

    v

--- Zelda/EndPointDetail/EndPointDetailPopupViewController.swift
+++ Zelda/EndPointDetail/EndPointDetailPopupViewController.swift
@@ -7,7 +7,7 @@
 
 import Cocoa
 
-let testRecordItem = RecordItem(duration: 100, statusCode: 8, time: Date(), requestHeader: "", responseHeader: "server:nginx/1.10.2\ndate:Tue, 15 Sep 2020 11:59:29 GMT\ncontent-type:text/plain\ncontent-length:110\nlast-modified:Sun, 16 Aug 2020 23:47:44 GMT\nconnection:close\netag:\"5f39c5a0-6e\"\naccept-ranges:bytes", responseBody: "", fields: [])
+let testRecordItem = RecordItem(duration: 100, statusCode: 8, time: Date(), requestHeader: "", responseHeader: "server:nginx/1.10.2\ndate:Tue, 15 Sep 2020 11:59:29 GMT\ncontent-type:text/plain\ncontent-length:110\nlast-modified:Sun, 16 Aug 2020 23:47:44 GMT\nconnection:close\netag:\"5f39c5a0-6e\"\naccept-ranges:bytes", responseBody: "{\"a\": 1, \"b\": {\"c\": 2}}", fields: [])
 
 class EndPointDetailPopupViewController: NSViewController {
 	// MARK: Internal
@@ -15,6 +15,7 @@ class EndPointDetailPopupViewController: NSViewController {
 	var recordItem: RecordItem = testRecordItem
 
 	@IBOutlet var headerView: NSGridView!
+	@IBOutlet var bodyView: NSTextField!
 
 	override func viewDidLoad() {
 		super.viewDidLoad()
@@ -26,11 +27,12 @@ class EndPointDetailPopupViewController: NSViewController {
 		for (k, v) in recordItem.responseHeader.dict {
 			appendRow(k, v)
 		}
+		bodyView.stringValue = recordItem.responseBody
 	}
 
 	// MARK: Private
 
 	private func appendRow(_ k: String, _ v: String) {
-		headerView.addRow(with: [NSTextField(labelWithString: k.capitalized), NSTextField(labelWithString: v)])
+		headerView.addRow(with: [NSTextField(labelWithString: k.capitalized), NSTextField(labelWithString: v.jsonPrettify ?? "")])
 	}
 }
--- Zelda/Tools/JSON.swift
+++ Zelda/Tools/JSON.swift
@@ -22,14 +22,18 @@ struct JSONFragment {
 }
 
 extension DateFormatter {
-  static let iso8601Full: DateFormatter = {
-	let formatter = DateFormatter()
-	formatter.dateFormat = "yyyy-MM-dd'T'HH:mm:ss.SSSZ"
-	formatter.calendar = Calendar(identifier: .iso8601)
-	formatter.timeZone = TimeZone(secondsFromGMT: 0)
-	formatter.locale = Locale(identifier: "en_US_POSIX")
-	return formatter
-  }()
+	static let iso8601Full: DateFormatter = {
+		let formatter = DateFormatter()
+		formatter.dateFormat = "yyyy-MM-dd'T'HH:mm:ss.SSSZ"
+		formatter.calendar = Calendar(identifier: .iso8601)
+		formatter.timeZone = TimeZone(secondsFromGMT: 0)
+		formatter.locale = Locale(identifier: "en_US_POSIX")
+		return formatter
+	}()
 }
 
-
+extension String {
+	var jsonPrettify: String? {
+		JSON(self).rawString(.utf8, options: [.prettyPrinted, .sortedKeys])
+	}
+}
